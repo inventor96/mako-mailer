51209b21df8ee2fc538e92c6a7e7077d947a33a4
Caleb Hornbeck <inventor96@gmail.com>
2026-01-29 04:03:20 -0700

    Register email sender adapter in MailerPackage bootstrap method

--- src/MailerPackage.php
+++ src/MailerPackage.php
@@ -1,6 +1,7 @@
 <?php
 namespace inventor96\MakoMailer;
 
+use inventor96\MakoMailer\interfaces\EmailSenderInterface;
 use mako\application\Package;
 
 class MailerPackage extends Package {
@@ -13,5 +14,9 @@ class MailerPackage extends Package {
 	function bootstrap(): void {
 		// register the mailer class as a singleton
 		$this->container->registerSingleton([Mailer::class, 'mailer'], Mailer::class);
+
+		// register the email sender adapter
+		$adapter_class = $this->container->get('config')->get("{$this->fileNamespace}::email.adapter");
+		$this->container->register(EmailSenderInterface::class, $adapter_class);
 	}
 }
